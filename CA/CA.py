e8deedb7571768b459e6ab181e47fd49f315837f
rouzbeh <rouzyd@gmail.com>
2021-07-15 04:40:01 +0430

    Completed SocketManager and CA

--- CA/CA.py
+++ CA/CA.py
@@ -1,17 +1,32 @@
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography import x509
-from cryptography.x509.oid import NameOID
-from cryptography.hazmat.primitives import hashes
+from SocketManager.SocketManager import SocketManager
 import datetime
 
 
 class CA:
-    def __init__(self):
+
+    def __init__(self, HOST, PORT):
         self.key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
         self.certs = dict()
+        self.client_server = SocketManager(HOST, PORT)
 
-    def get_cert(self, csr):
+    def run(self):
+        '''
+            you should send (host, port, message) as binary message to CA
+            after that, we convert it to object in run function
+        '''
+        data = self.client_server.receive_message()
+        host, port, message = self.client_server.convert_to_obj(data)
+
+    def send_cert(self, target_host, target_port, csr):
+        '''
+            use run function to get host, port, csr and then in this function you can send cert to host and port
+        '''
+
         subject = csr.subject
         issuer = csr.subject
 
@@ -34,7 +49,16 @@ class CA:
             # Sign our certificate with our private key
         ).sign(self.key, hashes.SHA256())
 
-        return cert
+        # add cert to self.certs
+        self.certs[cert.public_key] = cert
 
-    def get_public_key(self):
-        return self.key.public_key()
+        cert_binary = self.client_server.convert_to_binary(cert)
+
+        self.client_server.send_message(target_host, target_port, cert_binary)
+
+    def send_public_key(self, target_host, target_post):
+        '''
+            use run function firts to get host, port and after that send you public_key to that host and port
+        '''
+
+        self.client_server.send_message(target_host, target_post, self.key.public_key())
--- SocketManager/SocketManager.py
+++ SocketManager/SocketManager.py
@@ -1,64 +1,128 @@
 import socket
+import ssl
+import pickle
 
 
-def __init__(self, host, port, cert=None, key=None):
-    self.host = host
-    self.port = port
-    self.cert = cert
-    self.key = key
+class SocketManager:
 
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.CA_bundle = None
+        self.private_key = None
 
-def receive_message(self):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((self.host, self.port))
-        s.listen()
+    def receive_message(self):
+        '''
+            data is binary
+            you should convert it to obj by convert_to_obj function
+        '''
 
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.host, self.port))
+            s.listen()
+
+            print('server is listening...')
+            # print(self.host, self.port)
+            conn, addr = s.accept()
+
+            with conn:
+                print('Connected by', addr)
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+
+                    # hand shake, not necessary
+                    conn.sendall(data)
+
+                    # message = data.decode('utf-8')
+                    # print(message)
+
+                    return data
+
+    def send_message(self, HOST, PORT, message):
+        '''
+            HOST: target server host
+            PORT: target server port
+            message must be binary. before this you can convert your object with convert_to_binary function
+        '''
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+            s.connect((HOST, PORT))
+
+            s.sendall(message)
+
+            # hand shake, not necessary
+            data = s.recv(1024)
+
+            if data == message:
+                print('True handshake')
+
+            else:
+                print('False handshake')
+
+    def receive_with_ssl(self, ca_bundle, key):
+        '''
+            ca_bundle: it is an address from ca_bundle and must be string
+            key: it is key of any object that try to send message with ssl, it is an address
+        '''
+
+        self.ca_bundle = ca_bundle
+        self.key = key
+
+        serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serv = ssl.wrap_socket(serv, keyfile=key, certfile=ca_bundle, server_side=True)
+        serv.bind((self.host, self.port))
+        serv.listen()
         print('server is listening...')
-        # print(self.host, self.port)
-        conn, addr = s.accept()
+
+        conn, addr = serv.accept()
+        print('connected with ', addr)
 
         with conn:
             print('Connected by', addr)
+
             while True:
-                data = conn.recv(1024)
-                if not data:
+                from_client = conn.recv(4096)  # The received data type is byte, converted to str
+                if not from_client:
                     break
 
-                # hand shake, not necessary
-                conn.sendall(data)
+                conn.send(from_client)  # Convert str to byte type, you need to use byte when transmitting
 
-                message = data.decode('utf-8')
-                print(message)
+                conn.close()
+                print('client diconnected')
+                return from_client
 
-                return message
+    def send_with_ssl(self, ca_bundle, key, HOST, PORT, message):
+        '''
+            ca_bundle: it is an address from ca_bundle and must be string
+            key: it is key of any object that try to send message with ssl, it is an address
+            HOST: target host to send
+            PORT: target port to send
+            message: it must be binary.
+        '''
 
+        self.ca_bundle = ca_bundle
+        self.key = key
 
-def send_message(self, HOST, PORT, message):
-    '''
-        HOST: target server host
-        PORT: target server port
-    '''
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client = ssl.wrap_socket(client, keyfile=key, certfile=ca_bundle, server_side=False)
+        client.connect((HOST, PORT))
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        client.send(message)
 
-        s.connect((HOST, PORT))
+        from_server = client.recv(4096)
 
-        message_arr = bytes(message, 'utf-8')
-        s.sendall(message_arr)
-
-        # hand shake, not necessary
-        data = s.recv(1024)
-
-        if data == message_arr:
+        if from_server == message:
             print('True handshake')
-
         else:
             print('False handshake')
 
+        client.close()
 
-def send_with_ssl(self, host, port, message):
-    pass
+    def convert_to_binary(self, obj):
+        return pickle.dumps(obj)
 
-
-def receive_with_ssl(self):
-    pass
+    def convert_to_obj(self, binary):
+        return pickle.loads(binary)
